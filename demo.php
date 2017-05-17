f41fbe4b56076b8ff4399efe017772a2772d151d
houdunwang <2300071698@qq.com>
2017-05-17 11:45:07 +0800

    no message

--- demo.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$config = [
-	'dir' => 'log'
-];
-\houdunwang\config\Config::set( 'log', $config );
-\houdunwang\log\Log::write( '系统错误', \houdunwang\log\Log::ERROR );
